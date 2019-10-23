90471b24a7f92b5b866b9b6c2ad6913e7431ea94
weiBUAA <buaaxw@gmail.com>
2019-10-23 16:32:58 +0800

    first stable version of SVARM

--- src/offboard/src/offboard_node.cpp
+++ src/offboard/src/offboard_node.cpp
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
     ros::Rate rate(20.0);
 
     // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
+    while(ros::ok() && current_state.connected){
         ros::spinOnce();
         rate.sleep();
     }
@@ -61,23 +61,6 @@ int main(int argc, char **argv)
     ros::Time last_request = ros::Time::now();
 
     while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
-                ROS_INFO("Offboard enabled");
-            }
-            last_request = ros::Time::now();
-        } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
-                    ROS_INFO("Vehicle armed");
-                }
-                last_request = ros::Time::now();
-            }
-        }
 
         local_pos_pub.publish(pose);
 
