c87a364b551d3f0495f92547ede27325983af2d3
unknown <buaaxw@gmail.com>
2020-07-29 20:40:18 +0800

    change tool

--- tools/wsl2_ros_install.sh
+++ tools/wsl2_ros_install.sh
@@ -4,12 +4,11 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt upgrade
-sudo apt install ros-melodic-desktop-full catkin
+sudo apt-get install ros-melodic-desktop-full catkin ros-melodic-eigen3* ros-melodic-pcl* ros-melodic-sophus*
 sudo cp /etc/skel/.bashrc ~/
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 cd ~
 source ~/.bashrc
-sudo apt-get install ros-melodic-eigen3* ros-melodic-pcl* ros-melodic-sophus*
 mkdir Livox_ros_driver && cd Livox_ros_driver
 mkdir src && cd src
 git clone https://github.com/Livox-SDK/livox_ros_driver.git
